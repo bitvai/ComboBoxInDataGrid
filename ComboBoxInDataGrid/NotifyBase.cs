843effab8fbe50659be9f9b8dfb1ad33656a1975
Bitvai Csaba <csaba.bitvai@semilab.hu>
2023-08-31 10:06:29 +0200

    initial

---
+++ ComboBoxInDataGrid/NotifyBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ComboBoxInDataGrid
+{
+    public class NotifyBase : INotifyPropertyChanged
+    {
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        /// <summary>
+        /// Default version of OnPropertyChanged. Use this in usual cases
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            => RaisePropertyChanged(propertyName);
+
+        /// <summary>
+        /// Use this to raise a property changed event from code. Also override this if you need to inhibit or modify property change notification.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void RaisePropertyChanged(string propertyName, INotifyPropertyChanged source = null)
+            => PropertyChanged?.Invoke(source ?? this, new PropertyChangedEventArgs(propertyName ?? ""));
+
+        /// <summary>
+        /// Use this helper from property set bodies to get equality test and property change notification.
+        /// </summary>
+        /// <param name="field">a reference to the backing field of the property</param>
+        /// <param name="value">the new value of the property, "value" in the setter</param>
+        /// <param name="onChanged">action to execute if the property actually changed</param>
+        protected void Set<T>(ref T field, T value, Action onChanged = null, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                RaisePropertyChanged(propertyName);
+                onChanged?.Invoke();
+            }
+        }
+
+
+    }
+}
---
+++ ComboBoxInDataGrid/ViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBoxInDataGrid
+{
+    public enum Status
+    {
+        Status1,
+        Status2,
+        Status3
+    }
+
+    public class Data : NotifyBase
+    {
+        public string Name { get; private set; }
+
+        private Status _status;
+        public Status Status
+        {
+            get
+            {
+                return _status;
+            }
+            set
+            {
+                _status = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Data(string name)
+        {
+            Name = name;
+            Status = Status.Status1;
+        }
+    }
+
+    public class ViewModel : NotifyBase
+    {
+        private ObservableCollection<Data> _sokAdat;
+        public ObservableCollection<Data> SokAdat
+        {
+            get
+            {
+                return _sokAdat;
+            }
+            set
+            {
+                _sokAdat = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ViewModel()
+        {
+            SokAdat = new ObservableCollection<Data>();
+            SokAdat.Add(new Data("Data1"));
+            SokAdat.Add(new Data("Data2"));
+            SokAdat.Add(new Data("Data3"));
+            SokAdat.Add(new Data("Data4"));
+        }
+    }
+}
